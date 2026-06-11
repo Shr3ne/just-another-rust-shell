c335f2c58ab8f20c76f109e815609e61ca09c942
Shr3ne <shrinebloxianmail@gmail.com>
2026-06-11 23:50:00 +0530

    Added pipeline functionality

--- src/cmds.rs
+++ src/cmds.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::path::Path;
 use std::str::SplitWhitespace;
-use std::process::Command;
+use std::process::{Command, Stdio};
 use std::io::ErrorKind;
 
 pub fn handle_cd(args: SplitWhitespace) {
@@ -32,5 +32,56 @@ pub fn execute_external(cmd: &str, args: SplitWhitespace) {
                 println!("Failed to execute command: '{cmd}'");
             }
         }
+    }
+}
+
+pub fn execute_pipelines(pip_stages: Vec<String>) {
+    let mut previous_stdout: Option<Stdio> = None;
+    let mut children= Vec::new();
+    let total_stages = pip_stages.len();
+
+    for (i, stage) in pip_stages.iter().enumerate() {
+        let mut words = stage.split_whitespace();
+        let cmd = match words.next() {
+            Some(c) => c,
+            None => continue,
+        };
+
+        let args: Vec<&str> = words.collect();
+        let mut command = Command::new(cmd);
+        command.args(&args);
+
+        if let Some(stdin_source) = previous_stdout.take() {
+            command.stdin(stdin_source);
+        }
+
+        if i < total_stages - 1 {
+            command.stdout(Stdio::piped());
+        }
+
+        match command.spawn() {
+            Ok(mut child) => {
+                if i < total_stages - 1 {
+                    if let Some(stdout) = child.stdout.take() {
+                        previous_stdout = Some(Stdio::from(stdout));
+                    }
+                }
+                children.push(child);
+            }
+            Err(e) => {
+                if e.kind() == ErrorKind::NotFound {
+                    eprintln!("Pipeline command not found: '{cmd}'");
+                } else {
+                    eprintln!("Pipeline failed to execute: {e}");
+                }
+                return;
+            }
+        }
+
+        
+    }
+
+    for mut child in children {
+        let _ = child.wait();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,18 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        if input.is_empty() {
+        let trimmed_input = input.trim();
+        if trimmed_input.is_empty() {
+            continue;
+        }
+
+        if trimmed_input.contains('|') {
+            let pipeline_stage: Vec<String> = trimmed_input
+                    .split('|')
+                    .map(|s| s.to_string()) 
+                    .collect();
+
+            cmds::execute_pipelines(pipeline_stage);
             continue;
         }
 
